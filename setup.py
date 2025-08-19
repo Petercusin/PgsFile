b3e6e574c61dd07afcb35b919fba7ed63fee8b9d
Guisheng Pan <128466543+Petercusin@users.noreply.github.com>
2025-08-19 10:39:41 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -12,15 +12,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PgsFile",
-    version="0.2.4",
+    version="0.5.1",
     author="Pan Guisheng",
-    author_email="895284504@qq.com",
-    description="This module streamlines Python package management, script execution, file handling, web scraping, multimedia downloads, data cleaning, and NLP tasks such as word tokenization and POS tagging. It also assists with generating word lists and plotting data, making these tasks more accessible and convenient for literary students. Whether you need to scrape data from websites, clean text, or analyze language, this module provides user-friendly tools to simplify your workflow.",
+    author_email="panguisheng@sufe.edu.cn",
+    description="This module simplifies Python package management, script execution, file handling, web scraping, and multimedia downloads. The module supports (LLM-based) NLP tasks such as OCR, tokenization, lemmatization, POS tagging, NER, ATE, dependency parsing, MDD, WSD, LIWC, MIP analysis and Chinese-English sentence alignment. It also generates word lists, and plots data, aiding literary students. Ideal for scraping data, cleaning text, and analyzing language, it offers user-friendly tools to streamline workflows.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://mp.weixin.qq.com/s/12-KVLfaPszoZkCxuRd-nQ?token=1589547443&lang=zh_CN",
+    url="https://mp.weixin.qq.com/s/lWMkYDWQMjBJNKY2vMYTpw",
     license="Educational free",
-    install_requires=["chardet", "pandas", "python-docx", "pip", "requests", "fake-useragent", "lxml", "pimht", "pysbd", "nlpir-python","pillow"],
+    install_requires=["chardet", "pandas", "python-docx", "pip", "requests", "fake-useragent", "lxml", "pimht", "pysbd", "nlpir-python","pillow", "liwc"],
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
